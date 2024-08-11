791c1f928fa3174beb1a31538020be1954e18f0d
Akshay <akshay.mekkilery23@gmail.com>
2024-08-11 15:02:37 +0530

    React Component - Render Using class prop

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Car() {
-  return <h2>Hi, I am a Car!</h2>;
+function Car(props) {
+  return <h2>I am a {props.color} Car!</h2>;
 }
-const root = ReactDOM.createRoot(document.getElementById('MyRootDiv'));
-root.render(<Car />);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Car color="red"/>);
