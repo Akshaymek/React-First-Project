70adeb3330c4bbf7e43c2f0e61e9cfa4aed9d1dd
Akshay <akshay.mekkilery23@gmail.com>
2024-08-18 19:18:24 +0530

    Select

--- src/index.js
+++ src/index.js
@@ -2,42 +2,20 @@ import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 function MyForm() {
-  const [inputs, setInputs] = useState({});
-
+  const [myCar, setMyCar] = useState("Volvo");//take first option if ""
   const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
+      setMyCar(event.target.value)
+      alert("car:"+myCar);
   }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const { username, age } = inputs; // Destructure inputs to get username and age
-    alert(`Username: ${username}, Age: ${age}`);
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Enter your name:
-      <input 
-        type="text" 
-        name="username" 
-        value={inputs.username || ""} 
-        onChange={handleChange}
-      />
-      </label>
-      <label>Enter your age:
-        <input 
-          type="number" 
-          name="age" 
-          value={inputs.age || ""} 
-          onChange={handleChange}
-        />
-        </label>
-        <input type="submit" />
-    </form>
+      <form>
+      <select value={myCar} onChange={handleChange}>
+          <option value="Ford">Ford</option>
+          <option value="Volvo">Volvo</option>
+          <option value="Fiat">Fiat</option>
+      </select>
+      </form>
   )
 }
-
 const root = ReactDOM.createRoot(document.getElementById('MyRootDiv'));
 root.render(<MyForm />);
