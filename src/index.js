e3b278f83bf7ad6f3cee1e2f9206470c500a22a7
Akshay <akshay.mekkilery23@gmail.com>
2024-08-18 12:50:39 +0530

    Keys

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
 function Car(props) {
-  return <li>I am a { props.brand } &nbsp; owner:<strong>{props.owner}</strong></li>;
+  return <li>I am a {props.brand}</li>;
 }
 
-function Garage(){
-  const cars=['BMW','FORD','AUDI'];
+function Garage() {
+  const cars = [
+    { id: 1, brand: 'BMW' }, { id: 1, brand: 'FORD' }, { id: 1, brand: 'AUDI' }
+  ];
   console.log('cars:', cars);
-  return(
+  return (
     <>
-    <h1>LIST OF CARS</h1>
-    <ul>
-    {cars.map((car, index) => (
-          <Car key={index} brand={car} owner ='JOHN' />
-        ))}
+      <h1>LIST OF CARS</h1>
+      <ul>
+        {cars.map((car) => <Car  brand={car.brand} />)}
       </ul>
     </>
   );
 }
-
 const root = ReactDOM.createRoot(document.getElementById('MyRootDiv'));
 root.render(<Garage />);
